30369528e25b9cf2e9e72d069c1b23b6193a86a8
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-02 11:37:36 -0700

    Hello 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ *Description: prints"\"programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return(0);
 }
