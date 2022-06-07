af1981dbb3a931ddb52108da002e0caf2056bebb
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-07 10:01:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include "putchar.h"
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ * Return: 0.
+ */
+void print_alphabet(void)
+{
+	char sai;
+
+	for (sai = 'a'; sai <= 'z'; sai++)
+	{
+		_putchar(sai);
+	}
+	_putchar('\n');
+}
