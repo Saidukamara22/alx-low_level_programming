a3cfbee74ab96ad33a82cf04e3b238f1b1d3d3bc
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-07 05:04:40 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print putchar followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
