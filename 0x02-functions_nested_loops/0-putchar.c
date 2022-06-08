3bf35209a1b3554c8310022796fb184b4b390eb3
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-08 10:06:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char letter;
 
-	return (0);
+	for (letter = '0'; leter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
