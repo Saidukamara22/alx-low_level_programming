39f5d3d88acb470def981d7b08a611dbf2f8b614
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-08 06:57:05 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,13 @@
 /**
  * _abs - a function that compute the absolute value if an integer
  * @c: integer input
+ *
  * Return: Absolute value of ab
  */
 int _abs(int ab)
 {
-	return (ab * ((ab > 0) - (ab < 0)));
-}
+	if (ab >= 0)
+		return (ab);
+	else
+		return (-ab);
+} 
