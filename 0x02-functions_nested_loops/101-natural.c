db8933dfe9ac349718b73b178f145881e4b92759
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-08 10:26:39 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,24 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-	for (i = 0; i < 1024; i++)
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += 1;
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + 1;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + 1;
+		}
 	}
-
-	printf("%d\n", sum);
-
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
 	return (0);
 }
