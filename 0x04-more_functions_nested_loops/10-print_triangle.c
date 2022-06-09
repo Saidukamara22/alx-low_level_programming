fb636f5e25d4a9c642bd457864e7130bb11a781e
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-09 15:09:28 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,7 @@
 
 /**
  * print_triangle - a function that prints a triangle
- * @size: size of a triangle
+ * @size: size of the triangle
  * Return: triangle of '#'s
  */
 void print_triangle(int size)
