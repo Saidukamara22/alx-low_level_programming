26f57fd9e8ffceda6122cf83221b8035eb8bd92e
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-13 07:01:58 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i = 0;
+	int i = 0;
+	while (s[i])
+		i++;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; 1--)
+	while (i--)
+	{
 		_putchar(s[i]);
-
+	}
 	_putchar('\n');
 }
