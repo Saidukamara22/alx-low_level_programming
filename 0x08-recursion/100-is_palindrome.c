e1b401f3644150324fff72c15c93fa83d2e40097
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-06-22 10:00:19 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -38,7 +38,7 @@ int check_palindrome(char *s, int len, int index)
 	if (s[index] == s[len / 2])
 		return (1);
 
-	if (s[index] == s[len- index - 1])
+	if (s[index] == s[len - index - 1])
 		return (check_palindrome(s, len, index + 1));
 
 	return (0);
