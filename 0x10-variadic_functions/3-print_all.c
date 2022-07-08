00f0cb993801fc8a0a6d4eb9fffd86af8a2129c7
saidukamara22 <saiduabdulrahmankamara@gmail.com>
2022-07-08 01:23:59 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,7 @@
 
 /**
  * print_all - Prints anything.
- * @format: a list of types of arguments passed to the function.
+ * @format: A list of type of arguements passed to the function.
  *
  * Return: no return.
  */
@@ -10,12 +10,13 @@ void print_all(const char * const format, ...)
 {
 	va_list valist;
 	unsigned int i = 0, j, c = 0;
-	const char t_arg[] = "cifs";
+	char *str;
+	const char t_arg[] = "cifs';
 
 	va_start(valist, format);
 	while (format && format[i])
 	{
-		j - 0;
+		j = 0;
 		while (t_arg[j])
 		{
 			if (format[i] == t_arg[j] && c)
